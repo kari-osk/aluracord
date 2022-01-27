173833d8895f5b8c6dcef97300fe40114a400208
kari-osk <karinaosuka@gmail.com>
2022-01-27 14:46:43 -0300

    initial commit

---
+++ pages/chat.js
@@ -0,0 +1,229 @@
+import { Box, Text, TextField, Image, Button } from '@skynexui/components'
+import React from 'react'
+import appConfig from '../config.json'
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzI4NTQxMSwiZXhwIjoxOTU4ODYxNDExfQ.tk5Bp6_mYVZK2kxXxQmnt-x4U9J-RIkG_eL3Gvni0Mg'
+const SUPABASE_URL = 'https://qhjmkiormgtwqdfptgir.supabase.co'
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+
+export default function ChatPage() {
+  // Sua lógica vai aqui
+  //Usuário digita no campo de texto
+  // tem que adicionar enter para enviar
+  //tem que adicionar o texto na listagem
+
+  // Organize o que precisa fazer
+  //já tem campo para entrar texto?
+  //vamos usar onChange usa o useState(ter if para caso seja enter para limpar a variaval)
+  //listar as mensagens
+
+  const [mensagem, setMensagem] = React.useState('')
+  const [listaDeMensagens, setListaDeMensagens] = React.useState([])
+
+  React.useEffect(() => {
+    supabaseClient
+      .from('mensagens')
+      .select('*')
+      .order('id', { ascending: false })
+      .then(({ data }) => {
+        console.log('Dados da consulta:', data)
+        setListaDeMensagens(data)
+      })
+  }, [])
+
+  function handleNovaMensagem(novaMensagem) {
+    const mensagem = {
+      // id: listaDeMensagens.length + 1,
+      de: 'vanessametonini',
+      texto: novaMensagem
+    }
+    supabaseClient
+      .from('mensagens')
+      .insert([mensagem])
+      .then(({ data }) => {
+        console.log('Criando mensagem', data)
+        setListaDeMensagens([data[0], ...listaDeMensagens])
+      })
+
+    setMensagem('') // area para digitar volta a ficar vazia
+  }
+  return (
+    <Box
+      styleSheet={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/09/monsters-inc-doors-at-scare-floor-f.jpeg)`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundBlendMode: 'multiply',
+        color: appConfig.theme.colors.neutrals['000']
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: 'flex',
+          flexDirection: 'column',
+          flex: 1,
+          boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+          borderRadius: '5px',
+          backgroundColor: appConfig.theme.colors.neutrals[700],
+          height: '100%',
+          maxWidth: '95%',
+          maxHeight: '95vh',
+          padding: '32px'
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{
+            position: 'relative',
+            display: 'flex',
+            flex: 1,
+            height: '80%',
+            backgroundColor: appConfig.theme.colors.neutrals[600],
+            flexDirection: 'column',
+            borderRadius: '5px',
+            padding: '16px'
+          }}
+        >
+          <MessageList mensagens={listaDeMensagens} />
+          {/* Lista de mensagens:{' '}
+          {listaDeMensagens.map(mensagemAtual => {
+            console.log(mensagemAtual)
+            return (
+              <li key={mensagemAtual.id}>
+                {mensagemAtual.de}:{mensagemAtual.texto}
+              </li>
+            )
+          })} */}
+          <Box
+            as="form"
+            styleSheet={{
+              display: 'flex',
+              alignItems: 'center'
+            }}
+          >
+            <TextField
+              value={mensagem}
+              onChange={event => {
+                //()=> arrow function
+                console.log(event)
+                const valor = event.target.value
+                setMensagem(valor)
+              }}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  event.preventDefault() //o enter por padrão faz a quebra de linha, para evitar na area de mensagem colocar o preventDefault
+                  handleNovaMensagem(mensagem)
+                }
+              }}
+              placeholder="Insira sua mensagem aqui..."
+              type="textarea"
+              styleSheet={{
+                width: '100%',
+                border: '0',
+                resize: 'none',
+                borderRadius: '5px',
+                padding: '6px 8px',
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: '12px',
+                color: appConfig.theme.colors.neutrals[200]
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  )
+
+  function Header() {
+    return (
+      <>
+        <Box
+          styleSheet={{
+            width: '100%',
+            marginBottom: '16px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}
+        >
+          <Text variant="heading5">Chat</Text>
+          <Button
+            variant="tertiary"
+            colorVariant="neutral"
+            label="Logout"
+            href="/"
+          />
+        </Box>
+      </>
+    )
+  }
+
+  function MessageList(props) {
+    console.log('MessageList', props)
+    return (
+      <Box
+        tag="ul"
+        styleSheet={{
+          overflow: 'scroll',
+          display: 'flex',
+          flexDirection: 'column-reverse',
+          flex: 1,
+          color: appConfig.theme.colors.neutrals['000'],
+          marginBottom: '16px'
+        }}
+      >
+        {props.mensagens.map(mensagem => {
+          return (
+            <Text
+              key={mensagem.id}
+              tag="li"
+              styleSheet={{
+                borderRadius: '5px',
+                padding: '6px',
+                marginBottom: '12px',
+                hover: {
+                  backgroundColor: appConfig.theme.colors.neutrals[700]
+                }
+              }}
+            >
+              <Box
+                styleSheet={{
+                  marginBottom: '8px'
+                }}
+              >
+                <Image
+                  styleSheet={{
+                    width: '20px',
+                    height: '20px',
+                    borderRadius: '50%',
+                    display: 'inline-block',
+                    marginRight: '8px'
+                  }}
+                  src={`https://github.com/${mensagem.de}.png`}
+                />
+                <Text tag="strong">{mensagem.de}</Text>
+                <Text
+                  styleSheet={{
+                    fontSize: '10px',
+                    marginLeft: '8px',
+                    color: appConfig.theme.colors.neutrals[300]
+                  }}
+                  tag="span"
+                >
+                  {new Date().toLocaleDateString()}
+                </Text>
+              </Box>
+              {mensagem.texto}
+            </Text>
+          )
+        })}
+      </Box>
+    )
+  }
+}
